c26d6f6f5ceae8109ddd3f3a367b7fbac8ef0aa4
Iryna Kyryllova <keira.kirillova@gmail.com>
2024-10-03 19:57:24 +0200

    fix images api

--- src/api/images-api.js
+++ src/api/images-api.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+export const PER_PAGE = 12
+
 axios.defaults.baseURL = 'https://api.unsplash.com'
 axios.defaults.headers.common['Accept-Version'] = 'v1'
 
@@ -9,7 +11,7 @@ export const fetchImages = async (query, page) => {
       client_id: 'grwxOwNpWxADBOh-2dNzdUYrrBPS_Fy8Iza_RuWr9Pk',
       query,
       page,
-      per_page: 12
+      per_page: PER_PAGE
     }
   })
   return data
